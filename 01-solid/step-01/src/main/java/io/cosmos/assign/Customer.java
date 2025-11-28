36f381bd9f42af81305f076a1067730406da1158
Justin M4 <justinpark@Justins-MacBook-Pro.local>
2025-11-28 11:22:20 +0900

    SRP - 전화번호 값 객체 분리

--- 01-solid/step-01/src/main/java/io/cosmos/assign/Customer.java
+++ 01-solid/step-01/src/main/java/io/cosmos/assign/Customer.java
@@ -14,22 +14,24 @@ public class Customer {
 
     private Birthday birthday;
 
+    public Customer(String id, String name, String birthday, String gender, String landlinePhone, String mobilePhone) {
+        this.id = id;
+        this.name = name;
+        this.birthday = new Birthday(birthday);
+        this.landlinePhone = new Contact(Contact.ContactType.Landline, landlinePhone);
+        this.mobilePhone = new Contact(Contact.ContactType.Landline,mobilePhone);
+        this.gender = gender;
+    }
+
     /**
      *  연락처를 값 객체로 분리한다.
      *  1. 유선전화(LandLine, MobilePhone은 Enum으로 구분)
      */
-    private String landlinePhone;
-    private String mobilePhone;
+    private Contact landlinePhone;
+    private Contact mobilePhone;
     private String gender;
 
     private String surveyorId;
-
-    public Customer(String id, String name, String birthday, String gender) {
-        this.id = id;
-        this.name = name;
-        this.birthday = new Birthday(birthday);
-        this.gender = gender;
-    }
 
     public int getAge() {
         return this.getBirthday().getAge();
--- 01-solid/step-01/src/test/java/io/cosmos/assign/AssignServiceTest.java
+++ 01-solid/step-01/src/test/java/io/cosmos/assign/AssignServiceTest.java
@@ -14,11 +14,11 @@ public class AssignServiceTest {
     public void assign() {
         AssignService assignService = new AssignService();
 
-        Customer customer1 = new Customer("C1", "고객#1", "19911001", "W");
-        Customer customer2 = new Customer("C2", "고객#2", "19901001", "M");
-        Customer customer3 = new Customer("C3", "고객#3", "19891001", "M");
-        Customer customer4 = new Customer("C4", "고객#4", "19971001", "W");
-        Customer customer5 = new Customer("C5", "고객#5", "19921001", "M");
+        Customer customer1 = new Customer("C1", "고객#1", "19911001", "W", "","");
+        Customer customer2 = new Customer("C2", "고객#2", "19901001", "M", "","");
+        Customer customer3 = new Customer("C3", "고객#3", "19891001", "M", "","");
+        Customer customer4 = new Customer("C4", "고객#4", "19971001", "W", "","");
+        Customer customer5 = new Customer("C5", "고객#5", "19921001", "M", "","");
 
         List<Customer> customers = new ArrayList<>();
         customers.add(customer1);
